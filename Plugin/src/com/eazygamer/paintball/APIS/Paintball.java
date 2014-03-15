4c774ec9808cd506a58107982508423a2d009b00
EazyGamer <EazyGamer@Nathan>
2014-03-15 14:43:02 +0000

    'j

---
+++ Plugin/src/com/eazygamer/paintball/APIS/Paintball.java
@@ -0,0 +1,26 @@
+package com.eazygamer.paintball.APIS;
+
+public class Paintball {
+	private static final String Bukkit = null;
+	public void Bukkit(){
+		
+	}
+	
+	public void move(){
+
+		
+	}
+	public void rotate(){
+	}
+	public void options(){
+	}
+	public void settings(){
+	}
+	public void getResource(){
+	}
+	public void getResources(){
+		
+	}
+	
+
+}
---
+++ Plugin/src/com/eazygamer/paintball/paintball.java
@@ -0,0 +1,23 @@
+package com.eazygamer.paintball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class paintball extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	public void onDisable(){
+		
+	}
+	public void config(){
+		
+	}
+	public void teams(){
+		
+	}
+	public void gamemodes(){
+		
+	}
+
+}
